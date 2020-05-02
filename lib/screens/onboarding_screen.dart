16c990d2935e7aae7c122a44a467e267e2fffce0
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-02 18:24:32 +0430

    OnboardingScreen add some decoration

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -16,7 +16,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           "Here you can write the description of the page, to explain someting...",
       image: Center(child: Image.asset("assets/screen1.png", height: 175.0)),
       decoration: const PageDecoration(
-        pageColor: Colors.blue,
+        pageColor: Color(0xFF192A56),
+        bodyTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
     ),
     PageViewModel(
@@ -25,7 +33,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           "Here you can write the description of the page, to explain someting...",
       image: Center(child: Image.asset("assets/screen2.png", height: 175.0)),
       decoration: const PageDecoration(
-        pageColor: Colors.blue,
+        pageColor: Color(0xFF192A56),
+        bodyTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
     ),
     PageViewModel(
@@ -34,7 +50,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           "Here you can write the description of the page, to explain someting...",
       image: Center(child: Image.asset("assets/screen3.png", height: 175.0)),
       decoration: const PageDecoration(
-        pageColor: Colors.blue,
+        pageColor: Color(0xFF192A56),
+        bodyTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
       ),
     ),
   ];
@@ -51,9 +75,16 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           // You can also override onSkip callback
         },
         showSkipButton: true,
-        skip: Icon(Icons.skip_next),
-        next: Icon(Icons.arrow_right),
-        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        skip: Icon(
+          Icons.skip_next,
+          color: Colors.white,
+        ),
+        next: Icon(
+          Icons.arrow_right,
+          color: Colors.white,
+        ),
+        done: const Text("Done",
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
         dotsDecorator: DotsDecorator(
             size: const Size.square(10.0),
             activeSize: const Size(20.0, 10.0),
