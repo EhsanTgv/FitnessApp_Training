c829a7b5ddc6ab0a70d15ff587ad9897e1c8d81b
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-04 21:45:20 +0430

    HomePage implement navigation to ExerciseStartScreen

---
+++ lib/screens/exercise_start_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExerciseStartScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ExerciseStartScreenState();
+  }
+}
+
+class _ExerciseStartScreenState extends State<ExerciseStartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:fitnessapp/screens/exercise_hub.dart';
+import 'package:fitnessapp/screens/exercise_start_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -46,51 +47,60 @@ class _HomePageState extends State<HomePage> {
             ? ListView(
                 children: exerciseHub.exercises.map(
                   (exercise) {
-                    return Container(
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16)),
-                      child: Stack(
-                        children: <Widget>[
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(16),
-                            child: FadeInImage(
-                              image: NetworkImage(exercise.thumbnail),
-                              placeholder: AssetImage("assets/placeholder.jpg"),
-                              width: MediaQuery.of(context).size.width,
-                              height: 250,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(16),
-                            child: Container(
-                              width: MediaQuery.of(context).size.width,
-                              height: 250,
-                              decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                    colors: [
-                                      Color(0xff000000),
-                                      Color(0x00000000)
-                                    ],
-                                    begin: Alignment.bottomCenter,
-                                    end: Alignment.center),
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ExerciseStartScreen()));
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Stack(
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: FadeInImage(
+                                image: NetworkImage(exercise.thumbnail),
+                                placeholder:
+                                    AssetImage("assets/placeholder.jpg"),
+                                width: MediaQuery.of(context).size.width,
+                                height: 250,
+                                fit: BoxFit.cover,
                               ),
                             ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.bottomLeft,
-                            height: 250,
-                            child: Text(
-                              exercise.title,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 250,
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                      colors: [
+                                        Color(0xff000000),
+                                        Color(0x00000000)
+                                      ],
+                                      begin: Alignment.bottomCenter,
+                                      end: Alignment.center),
+                                ),
                               ),
                             ),
-                          )
-                        ],
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              alignment: Alignment.bottomLeft,
+                              height: 250,
+                              child: Text(
+                                exercise.title,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
