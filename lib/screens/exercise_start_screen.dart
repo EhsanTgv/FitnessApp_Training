ef9aef8f2b46a024f05ea020e1f2c47a972c0309
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-05 20:09:58 +0430

    ExerciseStartScreen add gradient

--- lib/screens/exercise_start_screen.dart
+++ lib/screens/exercise_start_screen.dart
@@ -21,21 +21,38 @@ class _ExerciseStartScreenState extends State<ExerciseStartScreen> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Hero(
-          tag: widget.exercises.id,
-          child: CachedNetworkImage(
-            imageUrl: widget.exercises.thumbnail,
-            placeholder: (context, url) => Image(
-              image: AssetImage("assets/placeholder.jpg"),
-              fit: BoxFit.cover,
-              height: 250,
-              width: MediaQuery.of(context).size.width,
-            ),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-            fit: BoxFit.cover,
-            height: 250,
-            width: MediaQuery.of(context).size.width,
-          ),
-        ),
+            tag: widget.exercises.id,
+            child: Stack(
+              children: <Widget>[
+                CachedNetworkImage(
+                  imageUrl: widget.exercises.thumbnail,
+                  placeholder: (context, url) => Image(
+                    image: AssetImage("assets/placeholder.jpg"),
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  fit: BoxFit.cover,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF000000),
+                        Color(0x00000000),
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.center,
+                    ),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
