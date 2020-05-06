3d55506877c8b58927a088f411ef97f0ea7760b0
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-06 14:12:54 +0430

    Implement ExerciseScreen class

---
+++ lib/screens/exercise_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExerciseScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ExerciseScreenState();
+  }
+}
+
+class _ExerciseScreenState extends State<ExerciseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
