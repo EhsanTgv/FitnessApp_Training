84170a688311545165e664b561c0fae72ef20dba
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-06 14:51:42 +0430

    ExerciseScreen passing & showing data

--- lib/screens/exercise_screen.dart
+++ lib/screens/exercise_screen.dart
@@ -1,6 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fitnessapp/screens/exercise_hub.dart';
 import 'package:flutter/material.dart';
 
 class ExerciseScreen extends StatefulWidget {
+  final Exercises exercises;
+
+  ExerciseScreen(this.exercises);
+
   @override
   State<StatefulWidget> createState() {
     return _ExerciseScreenState();
@@ -10,6 +16,18 @@ class ExerciseScreen extends StatefulWidget {
 class _ExerciseScreenState extends State<ExerciseScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl: widget.exercises.gif,
+          placeholder: (context, url) => Image(
+            image: AssetImage("assets/placeholder.jpg"),
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+      ),
+    );
   }
 }
